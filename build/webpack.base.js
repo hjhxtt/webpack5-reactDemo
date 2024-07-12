47659d37673097404aa8652659c85971338a3034
irving <497248252@qq.com>
2024-07-12 10:33:12 +0800

    修复打包bug

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -18,7 +18,7 @@ module.exports = {
     filename: 'static/js/[name].[chunkhash:8].js', // 每个输出js的名称  js使用chunkhash（js chunk改变才改变），图片字体类使用contenthash（文件改变才改变）
     path: path.resolve(__dirname, '../dist'), // 打包的出口文件夹路径
     clean: true, // webpack4需要配置clean-webpack-plugin删除dist文件，webpack5内置了。
-    publicPath: '/', // 打包后文件的公共前缀路径
+    publicPath: './', // 打包后文件的公共前缀路径
   },
   module: {
     rules: [
